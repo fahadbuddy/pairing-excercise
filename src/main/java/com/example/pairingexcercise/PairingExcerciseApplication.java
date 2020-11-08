e5d7f47c696b34898ef26aa2863afb136403fe4d
Fahad Sheikh <fahadbuddy@gmail.com>
2020-11-08 20:43:15 +0000

    first commit

---
+++ src/main/java/com/example/pairingexcercise/PairingExcerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.pairingexcercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairingExcerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairingExcerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pairingexcercise/PairingExcerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pairingexcercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairingExcerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
